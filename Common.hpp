5f34b7cb519e36095cff6a9cfab5f93efbd1ce7e
maxime334 <121265989+maxime334@users.noreply.github.com>
2023-06-23 15:12:41 -0400

    Update Common.hpp

--- Common.hpp
+++ Common.hpp
@@ -1,5 +1,20 @@
-#include <vector>
-#include <string>
 #include <iostream>
 #include <memory>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
 #pragma once
+
+namespace com {
+inline int pow(int x, int e) {
+  int r;
+  if (e == 0)
+    return 1;
+  if (e % 2 == 0) {
+    r = pow(x, e / 2);
+    return r * r;
+  } else
+    return x * pow(x, e - 1);
+}
+} // namespace com
